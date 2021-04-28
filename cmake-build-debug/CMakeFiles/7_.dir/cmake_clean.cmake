103bc2217d4cbfb5eb6a769cfb1662f6cd22080d
SXY <sxy1066740232@gmail.com>
2021-04-28 10:23:35 +0800

    v1.0

---
+++ cmake-build-debug/CMakeFiles/7_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "7_.exe"
+  "7_.exe.manifest"
+  "7_.pdb"
+  "CMakeFiles/7_.dir/main.c.obj"
+  "lib7_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/7_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//
+// Created by Sxing on 2021/04/28
+//
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
